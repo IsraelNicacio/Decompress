4ce851255f8120b955e8b22fd7b6015245ea18f2
Israel <israel.nicacio@outlook.com>
2020-01-21 09:10:35 -0300

    Add Decompress

---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X509Store st = new X509Store(StoreLocation.CurrentUser);
+            st.Open(OpenFlags.ReadOnly);
+            
+            foreach(X509Certificate2 cert in st.Certificates)
+            {
+                string[] arr = cert.Subject.Split(",");
+                for(int i = 0; i < arr.Length; i++)
+                {
+                    if(arr[i].Contains("INFOFISCO SISTEMAS"))
+                    {
+
+                    }
+                }
+            }
+
+            X509Certificate2Collection ret = (X509Certificate2Collection)st.Certificates.Find(X509FindType.FindBySubjectKeyIdentifier,"",false);
+            st.Close();
+
+            string fileName = @"C:\Xml\test.txt";
+            string file = File.ReadAllText(fileName);
+
+            //Trata xml
+            //var bytes  = Decompress(System.Convert.FromBase64String(file));
+            //var docXml = System.Text.Encoding.UTF8.GetString(bytes);
+            
+            XmlDocument xmlDoc = new XmlDocument();
+            //xmlDoc.LoadXml(docXml);
+            //xmlDoc.Save( @"C:\Xml\NFSe.xml");
+            
+            //Console.ReadKey();
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        public static byte[] Decompress(byte[] gzip)
+        {
+            // Create a GZIP stream with decompression mode.
+            // ... Then create a buffer and write into while reading from the GZIP stream.
+            using (GZipStream stream = new GZipStream(new MemoryStream(gzip), CompressionMode.Decompress))
+            {
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    return memory.ToArray();
+                }
+            }
+        }
+
+        public static string Zip(string value)
+        {
+            //Transform string into byte[]  
+            byte[] byteArray = new byte[value.Length];
+            int indexBA = 0;
+            foreach (char item in value.ToCharArray())
+            {
+                byteArray[indexBA++] = (byte)item;
+            }
+
+            //Prepare for compress
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            System.IO.Compression.GZipStream sw = new System.IO.Compression.GZipStream(ms,
+                System.IO.Compression.CompressionMode.Compress);
+
+            //Compress
+            sw.Write(byteArray, 0, byteArray.Length);
+            //Close, DO NOT FLUSH cause bytes will go missing...
+            sw.Close();
+
+            //Transform byte[] zip data to string
+            byteArray = ms.ToArray();
+            System.Text.StringBuilder sB = new System.Text.StringBuilder(byteArray.Length);
+            foreach (byte item in byteArray)
+            {
+                sB.Append((char)item);
+            }
+            ms.Close();
+            sw.Dispose();
+            ms.Dispose();
+            return sB.ToString();
+        }
+
+        public static string UnZip(string value)
+        {
+            //Transform string into byte[]
+            byte[] byteArray = new byte[value.Length];
+            int indexBA = 0;
+            foreach (char item in value.ToCharArray())
+            {
+                byteArray[indexBA++] = (byte)item;
+            }
+
+            //Prepare for decompress
+            System.IO.MemoryStream ms = new System.IO.MemoryStream(byteArray);
+            System.IO.Compression.GZipStream sr = new System.IO.Compression.GZipStream(ms,
+                System.IO.Compression.CompressionMode.Decompress);
+
+            //Reset variable to collect uncompressed result
+            byteArray = new byte[byteArray.Length];
+
+            //Decompress
+            int rByte = sr.Read(byteArray, 0, byteArray.Length);
+
+            //Transform byte[] unzip data to string
+            System.Text.StringBuilder sB = new System.Text.StringBuilder(rByte);
+            //Read the number of bytes GZipStream red and do not a for each bytes in
+            //resultByteArray;
+            for (int i = 0; i < rByte; i++)
+            {
+                sB.Append((char)byteArray[i]);
+            }
+            sr.Close();
+            ms.Close();
+            sr.Dispose();
+            ms.Dispose();
+            return sB.ToString();
+        }
+
+    }
+}
